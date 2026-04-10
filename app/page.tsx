23f38ba093c8e1cda9eaee6f4eba1ac110bcce39
InDemandAgency <hello@handsfreevas.com>
2026-04-10 15:15:57 +0300

    move plumbers landing page to /plumbers route

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,5 @@
-import Hero from "@/components/Hero";
-import WhoItsFor from "@/components/WhoItsFor";
-import ProofSection from "@/components/ProofSection";
-import ThreeProblems from "@/components/ThreeProblems";
-import HowWeSolve from "@/components/HowWeSolve";
-import HowItWorks from "@/components/HowItWorks";
-import NinetyDays from "@/components/NinetyDays";
-import Footer from "@/components/Footer";
+import { redirect } from "next/navigation";
 
-export default function Home() {
-  return (
-    <main>
-      <Hero />
-      <WhoItsFor />
-      <ProofSection />
-      <ThreeProblems />
-      <HowWeSolve />
-      <HowItWorks />
-      <NinetyDays />
-      <Footer />
-    </main>
-  );
+export default function RootPage() {
+  redirect("/plumbers");
 }
---
+++ app/plumbers/page.tsx
@@ -0,0 +1,23 @@
+import Hero from "@/components/Hero";
+import WhoItsFor from "@/components/WhoItsFor";
+import ProofSection from "@/components/ProofSection";
+import ThreeProblems from "@/components/ThreeProblems";
+import HowWeSolve from "@/components/HowWeSolve";
+import HowItWorks from "@/components/HowItWorks";
+import NinetyDays from "@/components/NinetyDays";
+import Footer from "@/components/Footer";
+
+export default function PlumbersPage() {
+  return (
+    <main>
+      <Hero />
+      <WhoItsFor />
+      <ProofSection />
+      <ThreeProblems />
+      <HowWeSolve />
+      <HowItWorks />
+      <NinetyDays />
+      <Footer />
+    </main>
+  );
+}
